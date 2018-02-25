3458206aff4a1f2ec42db4d630714dbdf599f98e
Brett Mack <brett.mack@contino.io>
2018-02-25 22:19:55 +0000

    Test deploy

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var server = require('./server');
-var port = process.env.PORT || 3000;
+// const awsServerlessExpress = require('aws-serverless-express');
+const server = require('./server');
+// var server = require('./server');
+const port = process.env.PORT || 3000;
 
 server.listen(port, function () {
   console.log('Server running on port %d', port);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
 var users = ['John', 'Betty', 'Hal'];
 
 app.get('/api/users', function (req, res) {
